80bde403f74c2505782b4a8991976fbe06a40a1d
ka1ea6 <kaleab77gulilat@gmail.com>
2022-09-24 15:30:37 +0300

    pep

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
         argQuant = "arguments" if len(sys.argv) > 2 else "argument"
         argPrint = "{0} {1}:".format(len(sys.argv) - 1, argQuant)
     else:
-        argPrint =  "0 arguments."
+        argPrint = "0 arguments."
     print(argPrint)
     for i in range(1, len(sys.argv)):
         print("{0}: {1}".format(i, sys.argv[i]))
