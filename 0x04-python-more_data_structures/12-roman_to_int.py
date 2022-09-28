3882e0fcd6a627d9043927d39917dd6dc3968924
ka1ea6 <kaleab77gulilat@gmail.com>
2022-09-28 21:11:49 +0300

    12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    dict = {
+        "I": 1,
+        "V": 5, 
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
+    }
+    sum = 0
+    last = 0
+    for i in roman_string[::-1]:
+        if last <= dict[i]:
+            sum += dict[i]
+        else:
+            sum -= dict[i]
+        last = dict[i]
+    return sum
