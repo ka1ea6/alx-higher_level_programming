a733c51107cdceb75e6c71dd168fcba011c3423b
ka1ea6 <kaleab77gulilat@gmail.com>
2022-10-10 17:56:23 +0300

    0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try: 
+        i = 0
+        while(my_list[i] and i < x):
+            i += 1
+        for j in range(i):
+            print("{0}".format(my_list[j]), end="")
+        print("")
+        return int(i)
+
+    except IndexError:
+        for j in range(i):
+            print("{0}".format(my_list[j]), end="")
+        print("")
+        return int(i)
