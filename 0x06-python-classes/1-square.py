de4b18f5ee271c8f9112f68cae578c835907daad
ka1ea6 <kaleab77gulilat@gmail.com>
2022-10-11 19:33:23 +0300

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''A module fo defining a square based on the passed size'''
+
+
+class Square:
+    '''A class that accepts a size as input and creates a square'''
+    def __init__(self, size):
+        self.__size = size
