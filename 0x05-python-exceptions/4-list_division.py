6ec0bee82793dad92f1a82d140f4914b85728689
ka1ea6 <kaleab77gulilat@gmail.com>
2022-10-11 22:07:07 +0300

    4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-from decimal import DivisionByZero
-from matplotlib.pyplot import draw_if_interactive
 
 
 def list_division(my_list_1, my_list_2, list_length):
@@ -11,7 +9,7 @@ def list_division(my_list_1, my_list_2, list_length):
                 if not my_list_1[i] or not my_list_2[i]:
                     raise IndexError
                 if my_list_2[i] == 0:
-                    raise DivisionByZero
+                    raise ZeroDivisionError
                 my_list1_type_check = isinstance(my_list_1[i], int)
                 my_list2_type_check = isinstance(my_list_2[i], int)
                 if not my_list1_type_check or not my_list2_type_check:
@@ -20,7 +18,7 @@ def list_division(my_list_1, my_list_2, list_length):
             except IndexError:
                 print("out of range")
                 res.append(0)
-            except DivisionByZero:
+            except ZeroDivisionError:
                 print("division by 0")
                 res.append(0)
             except TypeError:
