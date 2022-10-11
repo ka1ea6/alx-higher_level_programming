c3d199537d9d6e5d710a3c3fb7e0234b55af7cef
ka1ea6 <kaleab77gulilat@gmail.com>
2022-10-11 22:26:49 +0300

    100

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import stderr
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError as te:
+        print("Exception: {}".format(te), file=stderr)
+        return False
