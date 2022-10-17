2cbe407078bba35c1879de4d08743762bade1a61
ka1ea6 <kaleab77gulilat@gmail.com>
2022-10-17 22:34:14 +0300

    9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -98,3 +98,15 @@ class Rectangle:
 
     def __del__(self):
         print("Bye rectangle...")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an integer of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        return rect_2 if rect_1.area() < rect_2.area() else rect_1
+
+    @classmethod
+    def square(cls, size=0):
+        return cls(size, size)
