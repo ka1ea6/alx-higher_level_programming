1bbde3476ca28e45a3fa13ff6cdadda6f46a446a
ka1ea6 <kaleab77gulilat@gmail.com>
2022-10-19 18:04:24 +0300

    3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -2,15 +2,17 @@
 '''
 Module containing the function to print the full name of users
 '''
+
+
 def say_my_name(first_name, last_name=""):
     '''
-    Funciton to print the full name based on the given 
+    Funciton to print the full name based on the given
     first and last name
     '''
 
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    elif not isinstance(last_name,str):
+    elif not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
     print("My name is {} {}".format(first_name, last_name))
