9c7e043f2ac3ac91a4975401a6e9c9a641a65857
ka1ea6 <kaleab77gulilat@gmail.com>
2022-10-24 13:27:55 +0300

    0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+'''Module defining a lookup function used to list all attributes 
+and methods of an object
+'''
+
 
 def lookup(obj):
+    '''Function used to perform lookup on an object'''
     return dir(obj)
