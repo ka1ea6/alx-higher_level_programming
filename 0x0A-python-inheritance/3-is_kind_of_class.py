aabc422140ea4f49ad68f1484a78188d5594238e
ka1ea6 <kaleab77gulilat@gmail.com>
2022-10-24 17:02:04 +0300

    3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 '''Module for defining a function that checks if
  an object is an instance of a class'''
 
@@ -9,7 +9,7 @@ def is_kind_of_class(obj, a_class):
     Args:
         @obj: object to be checked
         @a_class: class to be checked
-        Return: True if class is exact instance, false otherwise.
+        Return: True if obj is instance of a_class, false otherwise.
     '''
     
     return isinstance(obj, a_class)
