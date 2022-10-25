3c5e10131e018eeaf21d8f2e26ce8cbea07c775e
ka1ea6 <kaleab77gulilat@gmail.com>
2022-10-25 20:48:54 +0300

    8 correction

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 '''Module for defining a function to serialize an object'''
 
-import json
-
 
 def class_to_json(obj):
     '''Function to return the serialized object'''
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
