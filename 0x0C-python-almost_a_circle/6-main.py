9048c6dd7d86194859adfaa18be4992feca5281c
ka1ea6 <kaleab77gulilat@gmail.com>
2022-11-03 22:24:12 +0300

    7

--- 0x0C-python-almost_a_circle/6-main.py
+++ 0x0C-python-almost_a_circle/6-main.py
@@ -4,10 +4,10 @@ from models.rectangle import Rectangle
 
 if __name__ == "__main__":
 
-    r1 = Rectangle(2, 3, 2, 2)
-    r1.display()
+    # r1 = Rectangle(2, 3, 2, 2)
+    # r1.display()
 
-    print("---")
+    # print("---")
 
-    r2 = Rectangle(3, 2, 1, 0)
+    r2 = Rectangle(3, 3, 1, 0)
     r2.display()
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -85,8 +85,14 @@ class Rectangle(Base):
         '''Method to print the rectangle instance with '#' characters'''
         if self.y > 0:
             [print() for _ in range(self.y)]
-        [print(' ' * self.x, '#' * self.width) if self.x > 0
-            else print('#' * self.width) for _ in range(self.height)]
+        for _ in range(self.height):
+            if self.x > 0:
+                [print(' ', end="") for x in range(self.x)]
+            [print('#', end="") for wid in range(self.width)]
+            print()
+
+        # [print(' ' * self.x, '#' * self.width) if self.x > 0
+        #     else print('#' * self.width,) for _ in range(self.height)]
 
     def update(self, *args, **kwargs):
         '''public method to assign an argument to the attributes'''
