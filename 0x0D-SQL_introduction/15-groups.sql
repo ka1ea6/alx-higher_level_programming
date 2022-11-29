c0fa8bf0c75e5c109020d73605880e2a3b0dc6bd
ka1ea6 <kaleab77gulilat@gmail.com>
2022-11-29 14:35:43 +0300

    corrections

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- SQL script to list the number of records with the same score in table second_table
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- SQL script to list all records of the table second_table of hbtn_0c_0 database
-SELECT score, name FROM second_table ORDER BY score DESC,  name DESC;
+SELECT score, name FROM second_table WHERE name != "" ORDER BY score DESC,  name DESC;
