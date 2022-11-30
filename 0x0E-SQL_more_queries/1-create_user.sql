6eb2dfde42f13425a32cf82ba046147ae77f9357
ka1ea6 <kaleab77gulilat@gmail.com>
2022-11-30 16:14:48 +0300

    1 correction

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- SQL script to create a MYSQL user with uid 'user_0d_1'
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILAGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
