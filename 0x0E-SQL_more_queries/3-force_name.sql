40832e7d581c3ec328d1092d9980a66c364dfe1c
ka1ea6 <kaleab77gulilat@gmail.com>
2022-11-30 16:41:35 +0300

    3

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,6 @@
 --- SQL Script to create the table force_name
 -- creates a table force_name
-CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL );
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT, 
+    name VARCHAR(256) NOT NULL 
+    );
