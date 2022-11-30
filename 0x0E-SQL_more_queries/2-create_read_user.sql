f5e6f7c854f5a05a4c434000cb910cab6dddab69
ka1ea6 <kaleab77gulilat@gmail.com>
2022-11-30 16:35:49 +0300

    comment

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,7 @@
 -- SQL script to create a database hbtn_0d_2 and the ser user_0d_2
+-- creates a database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- creates a user if it doesn't exist
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+-- grants permission to user user_0d_2
 GRANT SELECT ON 'hbtn_0d_2'.* TO user_0d_2@localhost;
