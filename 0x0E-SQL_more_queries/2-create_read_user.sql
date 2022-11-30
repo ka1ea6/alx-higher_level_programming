7a98557a16d7a6eeab6691e8af27538b383d92d6
ka1ea6 <kaleab77gulilat@gmail.com>
2022-11-30 16:29:09 +0300

    2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,3 @@
+-- SQL script to create a database hbtn_0d_2 and the ser user_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
