ab5a4f1ca2bd6498da40d6ee46a3fc223ae0c2fb
ka1ea6 <kaleab77gulilat@gmail.com>
2022-11-30 17:05:36 +0300

    STATES CORRECTION

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -3,6 +3,6 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- creates a table states with the specified constraints
 CREATE TABLE IF NOT EXISTS states(
-    id INT UNIQUE NOT NULL AUTO GENERATED PRIMARY KEY,
+    id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );
