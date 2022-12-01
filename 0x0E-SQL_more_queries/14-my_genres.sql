666f81bc67ef2c303dd30edc7585c3ccd334dfc2
ka1ea6 <kaleab77gulilat@gmail.com>
2022-12-01 11:57:52 +0300

    14 new correciton

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -9,10 +9,3 @@ SELECT tv_genres.name AS name
             WHERE title = "Dexter"
     )
     ORDER BY tv_genres.name;
-
--- using multiple joins
--- SELEC tv_genres.name AS name
---     FROM tv_show_genres
---     LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
---     LEFT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
---     WHERE tv_shows.title = "DEXTER";
