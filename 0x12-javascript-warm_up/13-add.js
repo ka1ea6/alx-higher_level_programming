72c2d28b512ebcfeafc9eea898ca673b58af0263
ka1ea6 <kaleab77gulilat@gmail.com>
2022-12-19 22:49:41 +0300

    100

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
 exports.add = function (num1, num2) {
-  if (!num1 || !num2 || typeof num1 !== 'number' || typeof num2 !== 'number')
-    return 0;
-  else return num1 + num2;
+  if (!num1 || !num2 || typeof num1 !== 'number' || typeof num2 !== 'number') { return 0; } else return num1 + num2;
 };
