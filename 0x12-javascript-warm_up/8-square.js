60095c4992d5d98f340ad09a4019403077693c19
ka1ea6 <kaleab77gulilat@gmail.com>
2022-12-19 22:18:18 +0300

    8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const xTimes = Math.floor(parseInt(process.argv[2]));
+if (isNaN(xTimes)) console.log("Missing size")
+else {
+    for (let i = 0; i < xTimes; i++) {
+        console.log("X".repeat(xTimes))
+    }
+}
