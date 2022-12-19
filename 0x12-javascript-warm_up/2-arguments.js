68f689325ad4ccdd43ff80f5e786d75a83f8931c
ka1ea6 <kaleab77gulilat@gmail.com>
2022-12-19 22:08:13 +0300

    3

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const argNum = process.argv.length;
+const count = process.argv.length;
 console.log(
-  argNum === 2
+  count === 2
     ? 'No argument'
-    : argNum === 3
+    : count === 3
     ? 'Argument found'
     : 'Arguments found'
 );
