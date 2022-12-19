b4edc4afe3137c9228c22c5acbd52a841e80bafa
ka1ea6 <kaleab77gulilat@gmail.com>
2022-12-19 22:16:07 +0300

    x times printer

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const xTimes = Math.floor(parseInt(process.argv[2]));
+if (isNaN(xTimes)) console.log("Missing number of occurrences")
+else {
+    for (let i = 0; i < xTimes; i++) {
+        console.log("C is fun")
+    }
+}
