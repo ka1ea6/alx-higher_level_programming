d0b244157565b6792c7c0e58fd09ffc4770e9d3d
ka1ea6 <kaleab77gulilat@gmail.com>
2022-12-20 14:44:30 +0300

    one

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/python
+'''Python script for connecting to a database and executing a
+fetch query'''
 import sys
 import MySQLdb
 
@@ -7,12 +9,15 @@ if len(sys.argv) > 3:
     passwd = sys.argv[2]
     db_name = sys.argv[3]
 
-
-    db = MySQLdb.connect(host="127.0.0.1", user=user_name, passwd=passwd, db=db_name)
+    db = MySQLdb.connect(host="127.0.0.1",
+                         user=user_name,
+                         passwd=passwd,
+                         db=db_name)
     cur = db.cursor()
     cur.execute('SELECT * FROM states;')
     states = cur.fetchall()
     for row in states:
         print(row)
 else:
-    print(f"Usage: ./0-select_states.py <mysql_username> <mysql_password> <database_name>")
+    print(f"Usage: ./0-select_states.py \
+    <mysql_username> <mysql_password> <database_name>")
