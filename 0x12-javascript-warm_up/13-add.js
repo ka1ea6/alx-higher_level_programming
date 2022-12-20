ff66b31262963c9d8ae69a308a0a7182cba03c14
ka1ea6 <kaleab77gulilat@gmail.com>
2022-12-20 09:15:47 +0300

    13 correction

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
 exports.add = function (num1, num2) {
-  if (!num1 || !num2 || typeof num1 !== 'number' || typeof num2 !== 'number') { return 0; } else return num1 + num2;
+  if (
+    (!num1 && typeof num1 !== 'number') ||
+    (!num2 && typeof num2 !== 'number')
+  ) {
+    return 0;
+  } else return num1 + num2;
 };
