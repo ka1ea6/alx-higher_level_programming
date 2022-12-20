78e1b191a5a73da3f2342e71d841d874eae934ea
ka1ea6 <kaleab77gulilat@gmail.com>
2022-12-20 15:09:09 +0300

    corrections

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-'''Python script for connecting to a database and executing a
-fetch query'''
+'''Python script to list all states with a
+name starting with N from the db hbtn_0e_0_usa'''
+
 import sys
 import MySQLdb
 
@@ -8,18 +9,17 @@ if len(sys.argv) > 3:
     user_name = sys.argv[1]
     passwd = sys.argv[2]
     db_name = sys.argv[3]
-
     db = MySQLdb.connect(host="127.0.0.1",
                          user=user_name,
                          passwd=passwd,
                          db=db_name)
     cur = db.cursor()
-    cur.execute('SELECT * FROM states;')
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%';")
     states = cur.fetchall()
     for row in states:
         print(row)
     cur.close()
     db.close()
 else:
-    print(f"Usage: ./0-select_states.py \
+    print(f"Usage: ./1-filter_states.py \
     <mysql_username> <mysql_password> <database_name>")
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-'''Python script for connecting to a database and executing a
-fetch query'''
+'''script that takes an argument and displays
+all values in the states table of hbtn_0e_0_usa where
+name matches the argument'''
+
 import sys
 import MySQLdb
 
-if len(sys.argv) > 3:
+if len(sys.argv) > 4:
     user_name = sys.argv[1]
     passwd = sys.argv[2]
     db_name = sys.argv[3]
-
+    state_name = sys.argv[4]
     db = MySQLdb.connect(host="127.0.0.1",
                          user=user_name,
                          passwd=passwd,
                          db=db_name)
     cur = db.cursor()
-    cur.execute('SELECT * FROM states;')
+    cur.execute("SELECT * FROM states WHERE name = {};".format(state_name))
     states = cur.fetchall()
     for row in states:
         print(row)
     cur.close()
     db.close()
 else:
-    print(f"Usage: ./0-select_states.py \
-    <mysql_username> <mysql_password> <database_name>")
+    print(f"Usage: ./2-my_filter_states.py \
+    <mysql_username> <mysql_password> <database_name> <state_name>")
