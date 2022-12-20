655ac90cf15d0e9c10cbbc7f06cc8d695b50afe0
ka1ea6 <kaleab77gulilat@gmail.com>
2022-12-20 14:40:56 +0300

    one

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import sys
+import MySQLdb
+
+if len(sys.argv) > 3:
+    user_name = sys.argv[1]
+    passwd = sys.argv[2]
+    db_name = sys.argv[3]
+
+
+    db = MySQLdb.connect(host="127.0.0.1", user=user_name, passwd=passwd, db=db_name)
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states;')
+    states = cur.fetchall()
+    for row in states:
+        print(row)
+else:
+    print(f"Usage: ./0-select_states.py <mysql_username> <mysql_password> <database_name>")
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,3 @@
+# Object Relational Mapping (ORM) in Python using SQLAlchemy
+
+## This project is an introduction to using SQLAlchemy to connect to and interact with a MySQL database.
