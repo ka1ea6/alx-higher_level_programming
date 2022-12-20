cbee58d6494c2dde9aff1d6e068bd3db078dd54e
ka1ea6 <kaleab77gulilat@gmail.com>
2022-12-20 11:58:13 +0300

    102 optional

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
+
+// const fs = require("fs/promises");
 const fs = require('fs');
 
 const [input1, input2, output] = process.argv.slice(2, 5);
@@ -6,6 +8,19 @@ const [input1, input2, output] = process.argv.slice(2, 5);
 if (!input1 || !input2 || !output) {
   console.log('Usage: ./102-concat.js <input file 1> <input file 2> <output file>');
 }
+
+// (async () => {
+//     try{
+//         const f1 = await fs.readFile(input1 ,{encoding: 'utf-8'});
+//         const f2 = await fs.readFile(input2, {encoding: "utf-8"});
+
+//         await fs.writeFile(output, `${f1}\n${f2}`, {encoding: 'utf-8'})
+
+//     } catch(err) {
+//         console.error(`Err ${err}`)
+//     }
+
+// })()
 
 fs.readFile(input1, { encoding: 'utf-8' }, (err, input1Data) => {
   if (!err) {
