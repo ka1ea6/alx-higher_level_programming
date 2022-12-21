680c3c21022bf5bc94df7ae570d7e5dfeda59102
ka1ea6 <kaleab77gulilat@gmail.com>
2022-12-21 09:25:10 +0300

    four

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''Script to list all cities from the database
+hbtn+_0e_4_usa'''
+
+import sys
+import MySQLdb
+
+if len(sys.argv) > 3:
+    user_name = sys.argv[1]
+    passwd = sys.argv[2]
+    db_name = sys.argv[3]
+    db = MySQLdb.connect(host="localhost",
+                         user=user_name,
+                         passwd=passwd,
+                         db=db_name)
+    cur = db.cursor()
+    query = "SELECT cities.id, cities.name, states.name\
+    FROM cities LEFT JOIN states ON cities.id = states.id \
+     ORDER BY %s;"
+    cur.execute(query, ("cities.id",))
+    states = cur.fetchall()
+    for row in states:
+        print(row)
+    cur.close()
+    db.close()
+else:
+    print(f"Usage: ./1-filter_states.py \
+    <mysql_username> <mysql_password> <database_name>")
