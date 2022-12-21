d956c77310a3c4cfafcd876434894c195debb90f
ka1ea6 <kaleab77gulilat@gmail.com>
2022-12-21 11:51:03 +0300

    12 init

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -21,14 +21,11 @@ if __name__ == "__main__":
         Session = sessionmaker(bind=engine)
         session = Session()
 
-        Loui = State(name="Louisiana")
-        session.add(Loui)
+        to_update = session.query(State).filter(State.id == '2').first()
+
+        to_update.name = 'New Mexico'
+
         session.commit()
-
-        state = session.query(State).order_by(State.id)\
-                                    .filter(State.name == "Louisiana").first()
-
-        print("Not found" if not state else state.id)
     else:
-        print(f"Usage: ./11-model_state_insert.py \
+        print(f"Usage: ./12-model_state_update_id_2.py \
         <mysql_username> <mysql_password> <database_name>")
