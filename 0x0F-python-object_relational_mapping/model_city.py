1b5f125e1ee9366854930c1ff54c6c9fcb31db8d
ka1ea6 <kaleab77gulilat@gmail.com>
2022-12-21 12:33:33 +0300

    pycode on models

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -8,15 +8,17 @@ from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-
 class City(Base):
     '''Class for representing the city table using the
     sqlalchemy ORM'''
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False, unique=True,autoincrement=True)
+    id = Column(Integer, primary_key=True,
+                nullable=False,
+                unique=True,
+                autoincrement=True)
     name = Column(String(length=128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
     state = relationship("State", back_populates="cities")
-    
+
     def __repr__(self):
         return f"{self.id} {self.name}"
