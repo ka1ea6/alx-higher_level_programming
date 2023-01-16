99b4f956d5390c6357793439bc2053961637b677
ka1ea6 <kaleab77gulilat@gmail.com>
2023-01-16 12:20:59 +0300

    5 corrections

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Bash script to send a POST request
-curl -sX POST -d '{"email": "test@gmail.com", "subject": "I will always be here for PLD"}'
+# Bash script to send a POST request
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
