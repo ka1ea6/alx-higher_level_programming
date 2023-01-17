2854e1c362035345567beedb128903cb6b95dc98
ka1ea6 <kaleab77gulilat@gmail.com>
2023-01-17 13:42:34 +0300

    0 fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     import urllib.request
 
     url = "https://alx-intranet.hbtn.io/status"
-    # req = urllib.request.Request(url)
+    req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as res:
         response_page = res.read()
         print("Body response:")
