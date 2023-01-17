b6919b3cbc468dcf66e50f88a7c0194b471bc422
ka1ea6 <kaleab77gulilat@gmail.com>
2023-01-17 14:42:02 +0300

    8 fix

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,10 +6,12 @@ if __name__ == "__main__":
     import sys
 
     q = ""
-    if sys.argv[1] is not None:
+    if len(sys.argv) > 1:
         q = sys.argv[1]
 
     res = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
 
     res_json = res.json()
+    if res_json is None:
+        print("No result")
     print(f"[{res_json.get('id')}] {res_json.get('name')}")
