26fdbfdfbbe1c035702530c5fec20a5eef857cae
ka1ea6 <kaleab77gulilat@gmail.com>
2023-01-17 10:37:01 +0300

    0 init

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Python Script to fetch from a given url
+using the urllib package'''
+
+import urllib.request
+
+url = "https://alx-intranet.hbtn.io/status"
+req = urllib.request.Request(url)
+with urllib.request.urlopen(req) as res:
+    response_page = res.read()
+    print(f'\t- type: {type(response_page)}')
+    print(f'\t- content: {response_page}')
+    print(f'\t- utf8 content: {response_page.decode("ascii")}')
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,3 @@
+# 0x11 Python Network 1
+
+- Working with python networking using urllib and requests packages
