702f66fc6406657beba332e46f3c87c60b17ae5b
ka1ea6 <kaleab77gulilat@gmail.com>
2023-01-17 03:50:46 -0800

    8 fix and permission change

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -12,6 +12,9 @@ if __name__ == "__main__":
     res = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
 
     res_json = res.json()
-    if res_json is None:
+    if res_json == {}:
         print("No result")
-    print(f"[{res_json.get('id')}] {res_json.get('name')}")
+    elif res_json.get("id") is None or res_json.get("name") is None:
+        print("Not a valid JSON")
+    else:
+        print(f"[{res_json.get('id')}] {res_json.get('name')}")
