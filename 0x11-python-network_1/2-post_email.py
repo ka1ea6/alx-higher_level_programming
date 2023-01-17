73820bba696331c8a3c495a7ef8b4b6ec594ea8e
ka1ea6 <kaleab77gulilat@gmail.com>
2023-01-17 10:49:03 +0300

    2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''python script that takes an email and
+sends a post requst to the passed url'''
+
+import urllib.request
+import sys
+
+url = sys.argv[1]
+email = sys.argv[2]
+
+req = urllib.request.Request(url, email=email)
+with urllib.request.urlopen(req) as res:
+    response_page = res.read()
+    print(response_page.decode("ascii"))
