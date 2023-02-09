1fec161b3a33cb4de49a8f0a67108d5483a917f4
ka1ea6 <kaleab77gulilat@gmail.com>
2023-02-09 12:27:25 +0300

    2 and 3

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const request = require("request");
+
+const url = process.argv[2];
+
+request.get(url, (err, res, body) => {
+  console.log(`code: ${res.statusCode}`);
+});
---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require("request");
+
+const movieId = process.argv[2];
+request.get(
+  `https://swapi-api.alx-tools.com/api/films/${movieId}`,
+  (err, res, body) => {
+    const parsed = JSON.parse(body);
+    console.log(parsed.title);
+  }
+);
