a14fa0add3e92d426031ed6ce79501e1f94dc42d
ka1ea6 <kaleab77gulilat@gmail.com>
2023-02-21 11:26:45 +0300

    0 to 4

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,5 @@
+$("#toggle_header").bind("click", function () {
+  if ($("header").hasClass("red"))
+    $("header").removeClass("red").addClass("green");
+  else $("header").removeClass("green").addClass("red");
+});
---
+++ 0x15-javascript-web_jquery/README.md
@@ -0,0 +1,3 @@
+# 0x15 - Javascript web Jquery
+
+- Getting caught up with using JQuery.
